9071289fbb466d694acb981c90eb215054a9ce96
Alberto Parziale <alberto.parziale@caffeina.com>
2017-12-15 17:41:31 +0100

    move setting to application support directory

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const _ = require('underscore')
 const DEBUG = false
 const CWD 			= process.cwd()
 const execOpts = { cwd: CWD, stdio:[0,1,2], sync: true } // stdio is only needed for execSync|spawn
-const settingsPath = path.join(__dirname, 'settings.json')
+const settingsPath = path.join(app.getPath('userData'), 'settings.json')
 const assetsDirectory = path.join(__dirname, 'assets')
 
 var settings = {}
